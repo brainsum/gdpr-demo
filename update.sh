8569d2be0936f0096d26f420413037aa6727510d
Peter Ponya <pedro@brainsum.com>
2018-02-22 15:25:19 +0100

    minor additions: git status

--- update.sh
+++ update.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+git status
 git pull
 composer update drupal/gdpr --with-dependencies
 cd web && ../vendor/bin/drush -y updb && ../vendor/bin/drush -y pmu gdpr gdpr_dump && ../vendor/bin/drush -y en gdpr 
+git status
+# @todo add changes automatically yo git
